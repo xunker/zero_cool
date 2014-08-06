2eb912e2a0d025d990ab21bb7fb36492e88e5997
Matthew Nielsen <xunker@pyxidis.org>
2014-08-06 17:56:58 -0600

    removed CRs from ruby method containers

--- lib/zero_cool/language/ruby/container/methods.rb
+++ lib/zero_cool/language/ruby/container/methods.rb
@@ -1,13 +1,13 @@
 class ZeroCool::Language::Ruby::Container::InstanceMethodWithArguments < ZeroCool::Language::Ruby::Container
   opening_text 'def __METHOD_NAME__ __METHOD_ARGUMENTS__'
-  closing_text "end\n"
+  closing_text 'end'
   parent_types [ :class ]
   type :method
 end
 
 class ZeroCool::Language::Ruby::Container::InstanceMethodWithNoArguments < ZeroCool::Language::Ruby::Container 
   opening_text 'def __METHOD_NAME__'
-  closing_text "end\n"
+  closing_text 'end'
   parent_type :class
   type :method
 end
